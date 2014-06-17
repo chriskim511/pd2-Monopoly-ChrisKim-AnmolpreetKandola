f80d32cf6e8de2272545224403d18dd66fba459b
chriskim511 <christopherkim511@gmail.com>
2014-06-16 23:43:31 -0400

    update

--- Property.java
+++ Property.java
@@ -1,20 +1,31 @@
 public class Property {
     
-    String name;
-    int value, rent;
+    String name, color;
+    int value, rent, houses;
     public Property() {
 	name = "unknown";
     }
-    public Property(String n, int v, int r) {
+    public Property(String n, int v, int r, String c) {
 	name = n;
 	value = v;
 	rent = r;
+	houses = 0;
+	color = c;
     }
     public int getValue() {
 	return value;
     }
+    public String getColor() {
+	return color;
+    }
     public int getRent() {
 	return rent;
+    }
+    public int getHouse() {
+	return houses;
+    }
+    public void addHouse() {
+	houses += 1;
     }
     public String toString() {
 	return name;
